b017849749022f22f722d4300b3072d57f9ad8a5
Larss_J <larss.jakobsons@icloud.com>
2022-07-04 09:58:46 +0300

    Commit #1

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from naff import Client, Button, ButtonStyles, CommandTypes, context_menu, prefixed_command, listen
+from dotenv import load_dotenv
+import os
+
+bot = Client(sync_interactions=True)
+
+@listen()
+async def on_startup():
+    print("Ready")
+    print(f"This bot is owned by {bot.owner}")
+
+load_dotenv()
+bot_TOKEN = os.environ['TOKEN']
+bot.start(bot_TOKEN)
