b16aa9e01b7f7b745c18847cf860600fc5ab0b3a
Larss_J <larss.jakobsons@icloud.com>
2022-07-04 10:07:05 +0300

    I made changes

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ import os
 
 bot = Client(sync_interactions=True)
 
+print("I made changes")
+
+
 @listen()
 async def on_startup():
     print("Ready")
     print(f"This bot is owned by {bot.owner}")
 
+
 load_dotenv()
-bot_TOKEN = os.environ['TOKEN']
+bot_TOKEN = os.environ["TOKEN"]
 bot.start(bot_TOKEN)
