a5cc975aa53d97e0461f5c911ba4440ca96a74a7
Larss_J <larss.jakobsons@icloud.com>
2022-07-04 15:09:39 +0300

    Commit #2

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
+# Bot made by using NAFF
+# pip install git+https://github.com/NAFTeam/NAFF@dev
+
 import os
-from naff import Client, Button, ButtonStyles, CommandTypes, context_menu, prefixed_command, listen
+import naff
+from naff import (
+    Client,
+    Button,
+    ButtonStyles,
+    CommandTypes,
+    slash_command,
+    slash_option,
+    OptionTypes,
+    InteractionContext,
+    context_menu,
+    listen,
+    Member,
+)
 from dotenv import load_dotenv
-import os
 
 bot = Client(sync_interactions=True)
-
-print("I made changes")
 
 
 @listen()
 async def on_startup():
     print("Ready")
     print(f"This bot is owned by {bot.owner}")
+
+
+@slash_command(name="challenge")
+@slash_option(
+    name="challenge",
+    description="Challenge someone to a battle!",
+    opt_type=OptionTypes.USER,
+    required=True,
+)
+async def challenge_user(ctx: InteractionContext, oponent: Member.user):
+    await ctx.send(f"{oponent.user.mention}, you have been challenged by {ctx.author.mention}")
 
 
 load_dotenv()
