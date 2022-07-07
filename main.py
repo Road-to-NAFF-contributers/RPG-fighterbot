f2a2448c0bb3e32c67ecd9baa9c8ebfb79162d6d
SuperCatRocket <94199511+SuperCatRocket@users.noreply.github.com>
2022-07-07 14:31:21 +0200

    Commit #6

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from naff import (
     ButtonStyles,
     slash_command,
     slash_option,
-    component_callback,
     ComponentContext,
     OptionTypes,
     InteractionContext,
@@ -38,12 +37,12 @@ async def challenge_user(ctx: InteractionContext, oponent: Member.user):
     mycomponents: list[ActionRow] = spread_to_rows(
         # TODO: Add a custom emoji
         Button(
-            custom_id="fight_button",
+            custom_id=f"fight_button_{oponent.id}",
             style=ButtonStyles.GREEN,
             label="Fight🗡",
         ),
         Button(
-            custom_id="deny_button",
+            custom_id=f"deny_button_{oponent.id}",
             style=ButtonStyles.RED,
             label="Deny❌",
         ),
@@ -51,15 +50,20 @@ async def challenge_user(ctx: InteractionContext, oponent: Member.user):
 
     message = await ctx.send(f"{oponent.user.mention}, you have been challenged by {ctx.author.mention}", components=mycomponents)
 
-#Fight button event callback
-@component_callback("fight_button")
-async def click_fight(ctx: ComponentContext):
-    await ctx.send(f"{ctx.author.mention} has accepted the challenge!")
+@listen()
+async def on_component(event: ComponentContext):
+    #Gets the Event.context of the button click
+    ctx = event.context
 
-#Deny button event callback
-@component_callback("deny_button")
-async def click_deny(ctx: ComponentContext):
-    await ctx.send(f"{ctx.author.mention} has denied the challenge!")
+    #Checks whether the button was the button of who was challenged
+    if (not ctx.custom_id.endswith(f"{ctx.author.id}")):
+        await ctx.send("This is not your button!", ephemeral=True)
+        return
+    else:
+        if ctx.custom_id.startswith("fight_button"):
+            await ctx.send(f"{ctx.author.mention} has accepted the challenge!")
+        elif ctx.custom_id.startswith("deny_button"):
+            await ctx.send(f"{ctx.author.mention} has denied the challenge!")
 
 load_dotenv()
 bot_TOKEN = os.environ["TOKEN"]
