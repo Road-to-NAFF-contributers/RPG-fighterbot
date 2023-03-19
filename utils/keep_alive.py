9f21c7e699064f66a50f1da23a41d7fff5f8f871
redninja9854 <100790587+redninja9854@users.noreply.github.com>
2023-03-19 13:57:30 +0000

    Change formatting
    Removed unnecessary whitespace, as advised in PEP, and changed multi-line comments to single line comments, because the comments were 1 line long

--- utils/keep_alive.py
+++ utils/keep_alive.py
@@ -5,19 +5,19 @@ from threading import Thread
 
 from flask import Flask
 
-SYNTAX                        = 'Syntax: python {0} <script>'
+SYNTAX = 'Syntax: python {0} <script>'
 WEB_SERVER_KEEP_ALIVE_MESSAGE = 'Keeping the repl alive!'
 
-flask: Flask          = Flask('replit_keep_alive')
-log:   logging.Logger = logging.getLogger('werkzeug')
+flask: Flask = Flask('replit_keep_alive')
+log: logging.Logger = logging.getLogger('werkzeug')
 
 @flask.route('/')
 def index() -> str:
-    """ Method for handling the base route of '/'. """
+    #Method for handling the base route of '/'.
     return WEB_SERVER_KEEP_ALIVE_MESSAGE
 
 def keep_alive() -> None:
-    """ Wraps the web server run() method in a Thread object and starts the web server. """
+    #Wraps the web server run() method in a Thread object and starts the web server.
     def run() -> None:
         log.setLevel(logging.ERROR)
         flask.run(host = '0.0.0.0', port = 8080)
